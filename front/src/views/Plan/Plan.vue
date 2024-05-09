6491b32fe397e772d795a9c2010e41f5b3ca4579
chenwch53 <1346569601@qq.com>
2024-05-09 17:31:03 +0800

    feat：综测方案页面的pdf内嵌预览

--- front/src/views/Plan/Plan.vue
+++ front/src/views/Plan/Plan.vue
@@ -1,11 +1,46 @@
-<script setup>
-
-</script>
-
 <template>
-
+  <div class="container">
+    <TopBar></TopBar>
+    <div class="pdfContainer">
+      <embed src="http://43.136.61.147:9000/plan/软件工程学院本科生奖学金综合素质测评实施方案0407.pdf" type="application/pdf" width="100%" height="100%" />
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<script setup>
+import TopBar from "../../components/TopBar.vue";
+</script>
 
+<style scoped>
+.container {
+  position: relative; /* 确保内部元素可以使用相对定位 */
+  display: flex;
+  flex-direction: column;
+  align-items: center; /* 水平居中 */
+  justify-content: flex-start; /* 顶部对齐 */
+  height: 100vh; /* 占满整个视口高度 */
+  padding: 20px; /* 设置内边距 */
+  box-sizing: border-box; /* 让 padding 不影响容器的尺寸 */
+
+  background-image: url("../../assets/background.jpg");
+
+}
+
+.pdfContainer {
+  width: 80%; /* 设置宽度为屏幕宽度的80% */
+  /* max-width: 800px; 最大宽度为800px */
+  height: 85vh; /* 高度自适应内容 */
+  border: 1px solid #4d4b4b; /* 边框样式 */
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* 添加阴影效果 */
+  /* overflow: auto; 添加滚动条 */
+
+  /* 设置绝对定位 */
+  position: absolute;
+  top: 100px;
+  bottom: 50px; /* 距离父容器底部的偏移量 */
+  left: 50%; /* 水平居中 */
+  transform: translateX(-50%); /* 居中对齐 */
+
+  /* 可以根据需要调整其他样式，比如内边距、背景颜色等 */
+}
 </style>
