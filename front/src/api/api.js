a69bd0cbd28b13d05e6d464f6f7a51d86a1a0e89
chenwch53 <1346569601@qq.com>
2024-05-14 17:45:06 +0800

    更新综测相关api

--- front/src/api/api.js
+++ front/src/api/api.js
@@ -27,6 +27,165 @@ function Login(params)
 
 /* 综测相关 */
 
+/* 无参，返回politics_standard表格的所有数据 */
+function selectPolitics()
+{
+  return request({
+  url: '',
+  method: 'GET',
+  mock: false,
+  }); 
+}
+
+/* 无参，返回practice_standard表格的所有数据 */
+function selectPractice()
+{
+  return request({
+  url: '',
+  method: 'GET',
+  mock: false,
+  }); 
+}
+
+/* 无参，返回socialWork_standard表格的所有数据 */  
+function selectSocialWork()
+{
+  return request({
+  url: '',
+  method: 'GET',
+  mock: false,
+  }); 
+}
+
+/* 无参，返回study_standard表格的所有数据 */
+function selectStudy()
+{
+  return request({
+  url: '',
+  method: 'GET',
+  mock: false,
+  });  
+}
+
+/**
+ * 提交综测填报信息politics表单
+ * 参数：params（politics表除主键外的所有字段）
+ * 返回：
+*/
+function submitPolitics(params)
+{
+  return request({
+    url: '',
+    method: 'POST',
+    mock: false,
+    params: params
+  })
+}
+
+/**
+ * 提交综测填报信息practice表单
+ * 参数：params（practice表除主键外的所有字段）
+ * 返回：
+*/
+function submitPractice(params)
+{
+  return request({
+    url: '',
+    method: 'POST',
+    mock: false,
+    params: params
+  })
+}
+
+/**
+ * 提交综测填报信息socialWork表单
+ * 参数：params（socialWork表除主键外的所有字段）
+ * 返回：
+*/
+function submitSocialWork(params)
+{
+  return request({
+    url: '',
+    method: 'POST',
+    mock: false,
+    params: params
+  })
+}
+
+/**
+ * 提交综测填报信息study表单
+ * 参数：params（study表除主键外的所有字段）
+ * 返回：
+*/
+function submitStudy(params)
+{
+  return request({
+    url: '',
+    method: 'POST',
+    mock: false,
+    params: params
+  })
+}
+
+/**
+ * 根据学生id返回politics综测填报信息
+ * 参数：params（SID）
+ * 返回：相关填报信息的列表
+*/
+function selectPoliticsRecords(params)
+{
+  return request({
+    url: '',
+    method: 'POST',
+    mock: false,
+    params: params
+  })
+}
+
+/**
+ * 根据学生id返回practice综测填报信息
+ * 参数：params（SID）
+ * 返回：相关填报信息的列表
+*/
+function selectPracticeRecords(params)
+{
+  return request({
+    url: '',
+    method: 'POST',
+    mock: false,
+    params: params
+  })
+}
+
+/**
+ * 根据学生id返回socialWork综测填报信息
+ * 参数：params（SID）
+ * 返回：相关填报信息的列表
+*/
+function selectSocialWorkRecords(params)
+{
+  return request({
+    url: '',
+    method: 'POST',
+    mock: false,
+    params: params
+  })
+}
+
+/**
+ * 根据学生id返回study综测填报信息
+ * 参数：params（SID）
+ * 返回：相关填报信息的列表
+*/
+function selectStudyRecords(params)
+{
+  return request({
+    url: '',
+    method: 'POST',
+    mock: false,
+    params: params
+  })
+}
 
 /* 分数相关 */
 
